1820919a5c3e17041f76ab17efa8886531837d0a
SandFlake <dalvigsara@gmail.com>
2018-04-27 12:41:08 +0200

    La till en bild för belöning och ändrat till Flow Layout i CRW2

---
+++ src/windows/ChildRewardWindow2.java
@@ -0,0 +1,156 @@
+package windows;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+
+import profiles.Account;
+import profiles.ChildProfile;
+import tasks.Chore;
+import tasks.Task;
+
+public class ChildRewardWindow2 extends JPanel implements ActionListener {
+
+	private JButton btnHome;
+	private JButton btnDustSymbol = new JButton();
+	private JFrame frame;
+	private JLabel lblReward1;
+	private JLabel lblChildName;
+	private JLabel lblPoints;
+	private JButton btnBeach;
+
+
+
+	private Account account;
+	private ChildProfile childProfile;
+	private int rewardPoints;
+	private ImageIcon beach;
+	private ImageIcon rewardActivity;
+	private JLabel lblTitle;
+
+
+	public ChildRewardWindow2() {
+		start();
+	}
+
+	public void start() {
+		// TODO Auto-generated method stub
+
+		frame = new JFrame();
+		frame.setBounds(0, 0, 400, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setTitle(" Reward Window ");
+		InitializeGUI();
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+	}
+
+	public void InitializeGUI() {
+		// TODO Auto-generated method stub
+
+		// Main
+		JPanel pnlMain = new JPanel();
+		Border border0 = BorderFactory.createTitledBorder("MainPanel");
+		pnlMain.setBorder(border0);
+		pnlMain.setBounds(6, 6, 381, 500);
+		pnlMain.setLayout(null);
+		pnlMain.setBackground(Color.YELLOW);
+
+		// Top
+		JPanel pnlTop = new JPanel();
+		Border border1 = BorderFactory.createTitledBorder("");
+		pnlTop.setBorder(border1);
+		pnlTop.setBounds(12, 17, 358, 80);
+		pnlTop.setLayout(null);
+		pnlTop.setBackground(Color.WHITE);
+
+		lblTitle = new JLabel("Möjliga Belöningar ");
+		lblTitle.setFont(new Font("SansSerif", Font.BOLD, 18));
+		lblTitle.setBounds(140, 30, 285, 20);
+		pnlTop.add(lblTitle);
+
+		btnHome = new JButton(" Hem ");
+		btnHome.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		btnHome.setBounds(10, 16, 90, 50);
+		pnlTop.add(btnHome);
+
+		// Middle Panel 
+		JPanel pnlInfo = new JPanel();
+		Border border2 = BorderFactory.createTitledBorder("");
+		pnlInfo.setBorder(border2);
+		pnlInfo.setBounds(12, 130, 358, 140);
+		pnlInfo.setLayout(new FlowLayout());
+		pnlInfo.setBackground(Color.WHITE);
+		
+
+		Border border3 = BorderFactory.createEtchedBorder();
+		lblReward1 = new JLabel(" Val 1:  ");
+		lblReward1.setFont(new Font("SansSerif", Font.CENTER_BASELINE, 12));
+		lblReward1.setBorder(border3);
+		pnlInfo.add(lblReward1);
+		
+		btnBeach.setIcon(new ImageIcon("images/beach.jpg"));
+		btnBeach.setBounds(10, 10, 30, 30);
+		pnlInfo.add(btnBeach);
+
+		//Bottom Panel
+
+		JPanel pnlBottom = new JPanel();
+		Border border4 = BorderFactory.createTitledBorder("BottomPanel");
+		pnlBottom.setBorder(border4);
+		pnlBottom.setBounds(12, 300, 358, 180);
+		pnlBottom.setLayout(new FlowLayout());
+		
+		pnlBottom.setBackground(Color.WHITE);
+
+//		btnDustSymbol.setIcon(new ImageIcon("images/NinjaReducedSize3.png"));
+//		pnlBottom.add(btnDustSymbol);
+		
+		lblPoints = new JLabel(" Sara Poäng: ");
+		lblPoints.setFont(new Font("SansSerif", Font.CENTER_BASELINE, 12));
+		lblPoints.setBorder(border3);
+		pnlBottom.add(lblPoints);
+		
+		
+
+		pnlMain.add(pnlTop);
+		pnlMain.add(pnlInfo);
+		pnlMain.add(pnlBottom);
+		frame.add(pnlMain);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+
+	}
+
+	public static void main(String[] args) {
+		ChildRewardWindow2 childReward = new ChildRewardWindow2();
+	}
+}
