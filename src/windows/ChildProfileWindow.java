8dccd1bbb59fbcc1a9de154dc3cfef66dc225f3b
Maida Sijaric <36196288+MlleFabulous@users.noreply.github.com>
2018-05-02 15:48:55 +0200

    lägger till childprofilewindow

--- src/windows/ChildProfileWindow.java
+++ src/windows/ChildProfileWindow.java
@@ -1,10 +1,43 @@
 package windows;
 
-public class ChildProfileWindow extends AbstractWindowUI{
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-	public ChildProfileWindow(DisplayWindow displayWindow) {
-		super("title");
-		// TODO Auto-generated constructor stub
+/**
+ * GUI for children's profile
+ * 
+ * @author Maida
+ * 
+ *         25/4: Reinstalled and retry. --- 27/4: Reinstalled, once again. Took
+ *         >5h. --- 2/5: Reinstalled. AGAIN.
+ *
+ */
+
+public class ChildProfileWindow extends JPanel {
+	private Account account;
+	private String name;
+
+	private JLabel lblTitleHome; // Title, in middle of top panel
+	private ImageIcon dustBallImage; // Profilepic, in the middle of middle panel
+	private JButton btnProfileSymbol; // Symbol, profilepic will be inside this button
+
+	private JLabel lblNumberOrder; //Number of tasks
+	private JLabel lblLocation;
+	private JLabel lblChore;
+	private JLabel lblPointsForChore;
+
+	/*
+	 * Three setters and three getters for the components inside of a task (L + C +
+	 * R)
+	 */
+
+	// Constructor
+	public ChildProfileWindow() {
+		// this.name = name;
+		start();
+
 	}
 
 }
